4e8b1f0ee25b075122e3203c1f97a548e68c4cf4
Vignesh <smartvignesh81245@email.com>
2025-05-23 13:06:57 +0530

    half

--- src/pages/SettingsPage/SettingsPage.tsx
+++ src/pages/SettingsPage/SettingsPage.tsx
@@ -1,7 +1,50 @@
 import React from 'react';
 
-const SettingsPage = () => {
-	return <div>SettingsPage</div>;
+const SettingsPage: React.FC = () => {
+	return <div>
+		<div>
+			<div className=" mt-4 ">
+			<button className=" m-4 text-[#9b111e] font-semibold hover:text-white py-2 px-4 border rounded border-[#9b111e]
+			    border-transparent 
+			    bg-white 
+			    hover:bg-gradient-to-br 
+			    hover:from-[#700808] 
+			    hover:via-[#a61c1c] 
+			    hover:to-[#d23c3c]"> 
+			  Account Settings
+			</button>				
+			<button className="m-4 text-[#9b111e] font-semibold hover:text-white py-2 px-4 border rounded border-[#9b111e]
+			    border-transparent 
+			    bg-white 
+			    hover:bg-gradient-to-br 
+			    hover:from-[#700808] 
+			    hover:via-[#a61c1c] 
+			    hover:to-[#d23c3c]"> 
+			  Notification
+			</button>				
+			<button className="m-4 text-[#9b111e] font-semibold hover:text-white py-2 px-4 border rounded border-[#9b111e]
+			    border-transparent 
+			    bg-white 
+			    hover:bg-gradient-to-br 
+			    hover:from-[#700808] 
+			    hover:via-[#a61c1c] 
+			    hover:to-[#d23c3c]"> 
+			  Privacy Policy
+			</button>				
+			<button className="m-4 text-[#9b111e] font-semibold hover:text-white py-2 px-4 border rounded border-[#9b111e]
+			    border-transparent 
+			    bg-white 
+			    hover:bg-gradient-to-br 
+			    hover:from-[#700808] 
+			    hover:via-[#a61c1c] 
+			    hover:to-[#d23c3c]"> 
+			  Terms of Service
+			</button>				
+				
+			</div>
+			
+		</div>
+	</div>;
 };
 
 export default SettingsPage;
