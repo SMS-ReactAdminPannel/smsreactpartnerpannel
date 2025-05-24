9a6d0d6720c37e27dadb97d48dbbd59bf2f7e0de
New Author Name <newemail@example.com>
2025-05-24 19:10:54 +0530

    Updated Code

--- src/pages/Customer Management/CustomerServiceDetails.tsx
+++ src/pages/Customer Management/CustomerServiceDetails.tsx
@@ -441,9 +441,9 @@ const CustomerServiceDetails = () => {
         
         {/* Pagination */}
         {filteredProducts.length > 0 && (
-          <nav className="flex items-center flex-column flex-wrap md:flex-row justify-between pt-4" aria-label="Table navigation">
-            <span className="text-sm font-normal text-black dark:text-black mb-4 md:mb-0 block w-full md:inline md:w-auto">
-              Showing <span className="font-semibold text-gray-900 dark:text-black">
+          <nav className="flex items-center flex-column flex-wrap md:flex-row justify-between p-4" aria-label="Table navigation">
+            <span className="text-sm font-normal pl-3.5 text-black dark:text-black mb-4 md:mb-0 block w-full md:inline md:w-auto">
+               <span className="font-semibold text-gray-900 dark:text-black">
                 {indexOfFirstItem + 1}-{Math.min(indexOfLastItem, filteredProducts.length)}
               </span> of <span className="font-semibold text-gray-900 dark:text-black">
                 {filteredProducts.length}
@@ -451,11 +451,11 @@ const CustomerServiceDetails = () => {
             </span>
             
             <ul className="inline-flex -space-x-px rtl:space-x-reverse text-sm h-8">
-              <li>
+              <li className='pr-3'>
                 <button
                   onClick={() => setCurrentPage(prev => Math.max(prev - 1, 1))}
                   disabled={currentPage === 1}
-                  className="flex items-center justify-center px-3 h-8 ms-0 leading-tight text-black bg-white border border-gray-300 rounded-s-lg hover:bg-gray-100 hover:text-gray-700 dark:bg-white dark:border-gray-700 dark:text-black  dark:hover:text-black disabled:opacity-50"
+                  className="flex items-center justify-center px-3 h-8 ms-0 leading-tight text-black bg-white border-gray-300  hover:bg-gray-100 hover:text-gray-700 dark:bg-white dark:border-gray-700 dark:text-black  dark:hover:text-black disabled:opacity-50"
                 >
                   Previous
                 </button>
@@ -480,7 +480,7 @@ const CustomerServiceDetails = () => {
                 <button
                   onClick={() => setCurrentPage(prev => Math.min(prev + 1, totalPages))}
                   disabled={currentPage === totalPages}
-                  className="flex items-center justify-center px-3 h-8 leading-tight text-gray-500 bg-white border border-gray-300 rounded-e-lg hover:bg-gray-100  dark:bg-white  dark:text-black  disabled:opacity-50"
+                  className="flex items-center justify-center px-3 h-8 leading-tight text-gray-500 bg-white border border-gray-300 hover:bg-gray-100  dark:bg-white  dark:text-black  disabled:opacity-50"
                 >
                   Next
                 </button>
