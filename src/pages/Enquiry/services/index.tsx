5afba6e385435b0864c066ce2fc2ab2665be96fe
suruthiga407! <suruthiga.yaho@gmail.com>
2025-06-19 14:21:23 +0530

    enquiryformcreated

--- src/pages/Enquiry/services/index.tsx
+++ src/pages/Enquiry/services/index.tsx
@@ -8,3 +8,13 @@ export const createEnquiry = async (data:string)=>{
     console.log('created successfully ',error)
     }
 }
+
+export const getEnquiry = async(params:string)=>{
+    try{
+        const response:any=await Client.partner.enquery.getAll(params)
+        return response ;
+    }
+    catch(error){
+       console.log('recived all fetched data',error);
+    }
+}
